73a506d54cd06cc937cf8e9687aa9df7604ff6c7
Akshay Aravindan <akshayaravindan@gmail.com>
2020-05-22 15:35:34 -0700

    Set up project

---
+++ src/test/java/com/akshay/ReactSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
