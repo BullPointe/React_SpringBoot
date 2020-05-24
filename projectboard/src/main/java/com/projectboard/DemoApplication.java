7cdb211895afb3ff9b27330efb25bda013f9cbd7
Akshay Aravindan <akshayaravindan@gmail.com>
2020-05-24 12:45:51 -0700

    Fix Problems

--- projectboard/src/main/java/com/projectboard/DemoApplication.java
+++ projectboard/src/main/java/com/projectboard/DemoApplication.java
@@ -1,6 +1,5 @@
 package com.projectboard;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
